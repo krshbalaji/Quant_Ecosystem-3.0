aba81e98d208dc5fe92ff42ab9512181f8c95a56
Balaji A K <balajia.kbalaji@gmail.com>
2026-05-22 07:17:19 +0530

    refactor: isolate optional telegram dependency from control package

--- quant_ecosystem/telegram_control/__init__.py
+++ quant_ecosystem/telegram_control/__init__.py
@@ -2,7 +2,11 @@
 
 from quant_ecosystem.telegram_control.command_handler import CommandHandler
 from quant_ecosystem.telegram_control.system_status_reporter import SystemStatusReporter
-from quant_ecosystem.telegram_control.telegram_bot import QuantTelegramBot
 
-__all__ = ["QuantTelegramBot", "CommandHandler", "SystemStatusReporter"]
+__all__ = ["CommandHandler", "SystemStatusReporter"]
 
+try:
+    from quant_ecosystem.telegram_control.telegram_bot import QuantTelegramBot
+    __all__.append("QuantTelegramBot")
+except ImportError:
+    QuantTelegramBot = None
