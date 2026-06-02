881cee89da31dcdccba731f2f084178d19b0c153
Balaji A K <balajia.kbalaji@gmail.com>
2026-06-02 08:10:37 +0530

    refactor: migrate federation audit registry to keyed registry foundation

--- quant_ecosystem/cognition/swarm/federation_audit_registry.py
+++ quant_ecosystem/cognition/swarm/federation_audit_registry.py
@@ -1,32 +1,25 @@
-from typing import Dict
+from quant_ecosystem.core.keyed_registry import (
+    KeyedRegistry,
+)
 
-from .execution_lineage import ExecutionLineage
+from .execution_lineage import (
+    ExecutionLineage,
+)
 
 
-class FederationAuditRegistry:
-
-    def __init__(self):
-        self._registry: Dict[str, ExecutionLineage] = {}
+class FederationAuditRegistry(
+    KeyedRegistry[
+        str,
+        ExecutionLineage
+    ]
+):
 
     def register(
         self,
         lineage: ExecutionLineage,
     ) -> None:
 
-        self._registry[
-            lineage.lineage_id
-        ] = lineage
-
-    def get(
-        self,
-        lineage_id: str,
-    ):
-
-        return self._registry.get(lineage_id)
-
-    def exists(
-        self,
-        lineage_id: str,
-    ) -> bool:
-
-        return lineage_id in self._registry
+        super().register(
+            lineage.lineage_id,
+            lineage,
+        )
